7d19339f53f7c350a2b2e1480e17b52a32a097ae
Nicolas Morales <0nicolasmorales@gmail.com>
2025-02-26 14:16:06 -0300

    FIX: Swagger date format

--- src/main/java/com/nicolas/Lavadero/dto/request/AppointmentDTOIn.java
+++ src/main/java/com/nicolas/Lavadero/dto/request/AppointmentDTOIn.java
@@ -1,6 +1,7 @@
 package com.nicolas.Lavadero.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -9,6 +10,10 @@ import java.time.LocalDateTime;
 @Data
 public class AppointmentDTOIn {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm")
+    @Schema(
+            type = "string",
+            pattern = "dd/MM/yyyy HH:mm"
+    )
     private LocalDateTime date;
 
     @NotNull
